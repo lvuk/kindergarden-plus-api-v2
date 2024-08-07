fc2d1f2154463b43d575e717f9ae2f42c358f46e
lvuk <luka.vuk456@gmail.com>
2024-08-07 09:35:42 +0200

    added check for already logged in

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,10 +1,11 @@
+/* eslint-disable unicorn/no-await-expression-member */
 /* eslint-disable @typescript-eslint/explicit-member-accessibility */
 import User from '#models/user'
 import RegisterValidator from '#validators/RegisterValidator'
 import { HttpContext } from '@adonisjs/core/http'
 
 export default class AuthController {
-  public async register({ request, auth, response }: HttpContextContract) {
+  public async register({ request, auth, response }: HttpContext) {
     const { PIN, email } = request.only(['PIN', 'email'])
 
     const checkUserExistence = async (field: string, value: string) => {
@@ -44,6 +45,10 @@ export default class AuthController {
     const { PIN, email, password } = request.only(['PIN', 'email', 'password'])
 
     const user = await User.verifyCredentials(email || PIN, password)
+
+    if ((await User.accessTokens.all(user)).length > 0) {
+      return response.status(400).json({ error: 'User already logged in' })
+    }
 
     if (!user) {
       return response.status(400).json({ error: 'Invalid credentials' })
